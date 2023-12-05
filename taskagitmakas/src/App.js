ceac287a581e5679ee6003601392fc276b225bd2
kubraterkes <96838549+kubraterkes@users.noreply.github.com>
2023-12-05 12:53:33 +0300

    05.12.23
    App.js commit

--- taskagitmakas/src/App.js
+++ taskagitmakas/src/App.js
@@ -1,158 +1,46 @@
-import {MyProgressBar} from "./MyProgressBar";
-import {Alert} from "react-bootstrap";
-import {useState} from "react";
+
+import React from "react";
 
 function App() {
-    const [totalCount, setTotalCount] = useState(0);
-    return (
-        <div className="App">
-            <Alert variant="success">
-                Total count: {totalCount}!
-            </Alert>
-            <MyProgressBar changeCount={setTotalCount} variant="info"/>
-            <MyProgressBar changeCount={setTotalCount} variant="warning"/>
-            <MyProgressBar changeCount={setTotalCount} variant="danger"/>
-            <MyProgressBar changeCount={setTotalCount} variant="dark"/>
-            <MyProgressBar changeCount={setTotalCount} variant="light"/>
+  return (<div>
+    <head>
+    <title>Rock Paper Scissors</title>
+    <link rel="stylesheet" href="styles/12-rock-paper-scissors.css"/>
+  </head>
+  <body>
+    <p class="title">Rock Paper Scissors</p>
+    <button class="move-button js-rock-button">
+      <img src="images/rock-emoji.png" class="move-icon"/>
+    </button>
 
-            let score = JSON.parse(localStorage.getItem('score')) || {
-  wins: 0,
-  losses: 0,
-  ties: 0
-};
+    <button class="move-button js-paper-button">
+      <img src="images/paper-emoji.png" class="move-icon"/>
+    </button>
 
-updateScoreElement();
+    <button class="move-button js-scissors-button">
+      <img src="images/scissors-emoji.png" class="move-icon"/>
+    </button>
 
-/*
-if (!score) {
-  score = {
-    wins: 0,
-    losses: 0,
-    ties: 0
-  };
-}
-*/
+    <p class="js-result result"></p>
+    <p class="js-moves"></p>
+    <p class="js-score score"></p>
 
-let isAutoPlaying = false;
-let intervalId;
+    <button onclick="
+      score.wins = 0;
+      score.losses = 0;
+      score.ties = 0;
+      localStorage.removeItem('score');
+      updateScoreElement();
+    " class="reset-score-button js-reset-score-button">Reset Score</button>
 
-//const autoPlay = () => {
-
-//};
-function autoPlay() {
-  if (!isAutoPlaying) {
-    intervalId = setInterval(() => {
-      const playerMove = pickComputerMove();
-      playGame(playerMove);
-    }, 1000);
-    isAutoPlaying = true;
-
-  } else {
-    clearInterval(intervalId);
-    isAutoPlaying = false;
-  }
-}
-
-document.querySelector('.js-rock-button')
-  .addEventListener('click', () => {
-    playGame('rock');
-  });
-
-document.querySelector('.js-paper-button')
-  .addEventListener('click', () => {
-    playGame('paper');
-  });
-
-document.querySelector('.js-scissors-button')
-  .addEventListener('click', () => {
-    playGame('scissors');
-  });
-
-document.body.addEventListener('keydown', (event) => {
-  if (event.key === 'r') {
-    playGame('rock');
-  } else if (event.key === 'p') {
-    playGame('paper');
-  } else if (event.key === 's') {
-    playGame('scissors');
-  }
-});
-
-function playGame(playerMove) {
-  const computerMove = pickComputerMove();
-
-  let result = '';
-
-  if (playerMove === 'scissors') {
-    if (computerMove === 'rock') {
-      result = 'You lose.';
-    } else if (computerMove === 'paper') {
-      result = 'You win.';
-    } else if (computerMove === 'scissors') {
-      result = 'Tie.';
-    }
-
-  } else if (playerMove === 'paper') {
-    if (computerMove === 'rock') {
-      result = 'You win.';
-    } else if (computerMove === 'paper') {
-      result = 'Tie.';
-    } else if (computerMove === 'scissors') {
-      result = 'You lose.';
-    }
+    <button class="auto-play-button" onclick="
+      autoPlay();
+    ">Auto Play</button>
     
-  } else if (playerMove === 'rock') {
-    if (computerMove === 'rock') {
-      result = 'Tie.';
-    } else if (computerMove === 'paper') {
-      result = 'You lose.';
-    } else if (computerMove === 'scissors') {
-      result = 'You win.';
-    }
-  }
+    <script src="scripts/12-rock-paper-scissors.js"></script>
+  </body>
 
-  if (result === 'You win.') {
-    score.wins += 1;
-  } else if (result === 'You lose.') {
-    score.losses += 1;
-  } else if (result === 'Tie.') {
-    score.ties += 1;
-  }
-
-  localStorage.setItem('score', JSON.stringify(score));
-
-  updateScoreElement();
-
-  document.querySelector('.js-result').innerHTML = result;
-
-  document.querySelector('.js-moves').innerHTML = `You
-<img src="images/${playerMove}-emoji.png" class="move-icon">
-<img src="images/${computerMove}-emoji.png" class="move-icon">
-Computer`;
-}
-
-function updateScoreElement() {
-  document.querySelector('.js-score')
-    .innerHTML = `Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`;
-}
-
-function pickComputerMove() {
-  const randomNumber = Math.random();
-
-  let computerMove = '';
-
-  if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerMove = 'rock';
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerMove = 'paper';
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerMove = 'scissors';
-  }
-
-  return computerMove;
-}
-        </div>
-    );
+  </div>);
 }
 
 export default App;
