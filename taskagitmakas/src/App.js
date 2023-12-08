1ac4e9605a1a02501fffcfb3f91d28fedd6dd860
kubraterkes <96838549+kubraterkes@users.noreply.github.com>
2023-12-08 15:29:10 +0300

    08.12.2023
    yapı değişimi

--- taskagitmakas/src/App.js
+++ taskagitmakas/src/App.js
@@ -1,58 +1,10 @@
-import React, { useState, useEffect } from "react";
-import './App.css'; // Bu dosyayı oluşturduğunuz CSS dosyası ile değiştirin
+import './App.css';
+import Game from './components/Game';
 
 function App() {
-  const [score, setScore] = useState({
-    wins: 0,
-    losses: 0,
-    ties: 0
-  });
-
-  useEffect(() => {
-    updateScoreElement();
-  }, [score]);
-
-  const [isAutoPlaying, setIsAutoPlaying] = useState(false);
-  const [intervalId, setIntervalId] = useState(null);
-
-  const autoPlay = () => {
-    if (!isAutoPlaying) {
-      const newIntervalId = setInterval(() => {
-        const playerMove = pickComputerMove();
-        playGame(playerMove);
-      }, 1000);
-      setIsAutoPlaying(true);
-      setIntervalId(newIntervalId);
-    } else {
-      clearInterval(intervalId);
-      setIsAutoPlaying(false);
-    }
-  };
-
-  const playGame = (playerMove) => {
-    // Oyunu oynayan fonksiyon
-    // ...
-  };
-
-  const updateScoreElement = () => {
-    // Skorun ekrandaki gösterimini güncelleyen fonksiyon
-    // ...
-  };
-
-  const pickComputerMove = () => {
-    // Bilgisayarın rastgele hamle seçtiği fonksiyon
-    // ...
-  };
-
   return (
-    <div className="container">
-      <h1>Welcome to Rock, Paper, Scissors Game</h1>
-      <div>
-        {/* Butonlar ve diğer elementler */}
-      </div>
-      <div className="content">
-        {/* Skor ve oyun sonucu gösterimi */}
-      </div>
+    <div className="App">
+    <Game/>
     </div>
   );
 }
